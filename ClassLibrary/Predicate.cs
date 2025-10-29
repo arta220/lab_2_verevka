bf2f270484c9e7bad7819f41af605a2574cef52e
maggson <dieseverycingein@yandex.ru>
2025-10-30 02:18:53 +0300

    накиал очень примерную библу

---
+++ ClassLibrary/Predicate.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    // класс предиката
+    // ожидаются методы:
+    //определние вида предиката
+    //методы для работы с операциями в предикатах
+
+    public class Predicate
+    {
+    }
+}
