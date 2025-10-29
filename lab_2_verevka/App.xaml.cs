11f30e2590838e2dbee9a01851931e284826867b
maggson <dieseverycingein@yandex.ru>
2025-10-29 19:06:18 +0300

    Добавьте файлы проекта.

---
+++ lab_2_verevka/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace lab_2_verevka
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
